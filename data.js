c14b1c696bf9e399310ea1c4c5fb504fa07b4dcd
Hemant <hemantsingh1612@gmail.com>
2017-12-25 17:30:08 +0530

    added eslint

--- data.js
+++ data.js
@@ -1,72 +1,67 @@
-//1
-range = (start,end) => {
-  arr= [];
-  for(let i=start;i<=end;i++){
-    arr.push(i);
+// 1
+range = (start, end) => {
+  arr = []
+  for (let i = start; i <= end; i++) {
+    arr.push(i)
   }
-  return arr;
+  return arr
 }
-//1.1
-range = (start,end,step=1) => {
-  arr= [];
-  for(let i=start;i<=end;i+=step){
-    arr.push(i);
+// 1.1
+range = (start, end, step = 1) => {
+  arr = []
+  for (let i = start; i <= end; i += step) {
+    arr.push(i)
   }
-  return arr;
+  return arr
 }
 
-//2 using map
+// 2 using map
 
 sum1 = (arr) => {
-  let s = 0;
-  arr.map( (val) =>
-  s+= val )
-  return s;
+  let s = 0
+  arr.map((val) =>
+    s += val)
+  return s
 }
-//2 using reduce
+// 2 using reduce
 
-sum2 = (arr) => arr.reduce( (acc, val) =>  acc+val,0 )
+sum2 = (arr) => arr.reduce((acc, val) => acc + val, 0)
 
-//3
+// 3
 
-function arraytoList(arr,i=0){
-  if( i < arr.length-1){
-    return {
-      value : arr[i],
-      next: arrtoList(arr,i+1)
-    }
-  }
-  if(i == arr.length-1){
+function arraytoList (arr, i = 0) {
+  if (i < arr.length - 1) {
     return {
       value: arr[i],
-      next : null
+      next: arrtoList(arr, i + 1)
+    }
+  }
+  if (i == arr.length - 1) {
+    return {
+      value: arr[i],
+      next: null
     }
   }
 }
 
-
-//4
-function listtoArray(list){
-  ar = [];
-  while(true) {
-    ar.push(list.value);
-    list = list.next;
-    if(list.next === null ) {
-       ar.push(list.value);
-       break;
-     }
+// 4
+function listtoArray (list) {
+  ar = []
+  while (true) {
+    ar.push(list.value)
+    list = list.next
+    if (list.next === null) {
+      ar.push(list.value)
+      break
+    }
   }
-  return ar;
+  return ar
 }
 
+// 5
 
-//5
-
-function deepEqualObject(obj1,obj2){
-  for(prop in obj1){
-    if(prop in obj2 )
-     console.log('tes');
+function deepEqualObject (obj1, obj2) {
+  for (prop in obj1) {
+    if (prop in obj2) { console.log('tes') }
   }
-
-
 }
--- function.js
+++ function.js
@@ -1,27 +1,27 @@
-//1
-function recursive(num){
-  if(num == 0){
+// 1
+function recursive (num) {
+  if (num == 0) {
     return 'even';
   }
-  else if(num == 1){
+  else if (num == 1) {
     return 'odd';
   }
-  //only for num > 0
-  else return recursive(num - 2);
+  // Only for num > 0
+  return recursive(num - 2);
 }
 
 
-//2
+// 2
 
-countB = (str,chr = 'B') => {
-   let count =0;
-   let code = chr.charCodeAt(0)
-   for(let i=0;i<str.length;i++){
-     if(str.charCodeAt(i) == code || str.charCodeAt(i) == code ){
+countB = (str, chr = 'B') => {
+   let count = 0;
+   const code = chr.charCodeAt(0)
+   for (let i = 0; i < str.length; i++) {
+     if (str.charCodeAt(i) == code || str.charCodeAt(i) == code) {
        count++; }
      }
    return count;
 }
 
-//3
-countAnyChr = (str,chr) => countB(str,chr)
+// 3
+countAnyChr = (str, chr) => countB(str, chr)
